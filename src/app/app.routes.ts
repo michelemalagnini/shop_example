af1e0eed6cefcd7de442e07cbe0c0cb04c777919
michele malagnini <michelemalagn@gmail.com>
2025-01-11 16:31:31 +0100

    add cart and home components with routing; disable CLI analytics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { CartComponent } from './features/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'cart', component: CartComponent },
+];
