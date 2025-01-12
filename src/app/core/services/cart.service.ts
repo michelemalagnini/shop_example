27ac7e31345217298f2514c3d4d127a5b0b7aff6
michele malagnini <michelemalagn@gmail.com>
2025-01-12 15:39:17 +0100

    implement cart service methods to add items and calculate total cost

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { CartItem } from '../../app.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  items: CartItem[] = [];
 
-  constructor() { }
+  addToCart(cartitem: CartItem) {
+    if (cartitem) {
+      this.items = [...this.items, cartitem];
+    }
+  }
+  getTotalCart() {
+    return this.items.reduce((acc: number, curr: CartItem) => {
+      return acc + curr.product.cost;
+    }, 0);
+  }
 }
