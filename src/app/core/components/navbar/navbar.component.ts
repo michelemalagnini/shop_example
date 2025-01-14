2f5fc4a6410f48b7f1038a15638b880c1a2d3129
michele malagnini <michelemalagn@gmail.com>
2025-01-14 18:42:20 +0100

    implement custom modal for cart summary in navbar; add open/close functionality and styling

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,16 +1,33 @@
 import { Component, inject, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CartService } from '../../services/cart.service';
+import { NgFor, NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [NgIf, NgFor],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   @Input() title?: string;
-
   cartService = inject(CartService);
+  router = inject(Router);
+
+  showModal = false;
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  navigateToCart() {
+    this.closeModal(); // chiude la modale
+    this.router.navigate(['/cart']);
+  }
 }
